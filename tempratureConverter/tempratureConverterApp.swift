5bdd0ebfd26ed734f0b619bf6368dafa6602dc33
tanmayagrwl <tanmayhp78@gmail.com>
2024-11-16 19:50:36 +0530

    Initial Commit

---
+++ tempratureConverter/tempratureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempratureConverterApp.swift
+//  tempratureConverter
+//
+//  Created by Tanmay Agrawal on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct tempratureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tempratureConverterTests/tempratureConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  tempratureConverterTests.swift
+//  tempratureConverterTests
+//
+//  Created by Tanmay Agrawal on 16/11/24.
+//
+
+import Testing
+@testable import tempratureConverter
+
+struct tempratureConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
