7b197829f1a4968970d5e4c51350d94c843ebc23
tanmayagrwl <tanmayhp78@gmail.com>
2024-12-04 15:08:41 +0530

    add conversion logic

--- tempratureConverter/ContentView.swift
+++ tempratureConverter/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputTemp = 0.0
+    @State private var inputTempratureFormat = "K"
+    @State private var outputTempratureFormat = "K"
+    @State private var middleTemp = 0.0
+    let typesOfTemp = ["°F","°C", "K"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        var outputTemp:Double{
+            if(inputTempratureFormat == "°C" ){
+                middleTemp = inputTemp*1.8 + 32
+            } else if(inputTempratureFormat == "K"){
+                middleTemp = (inputTemp - 273.15 ) * 1.8 + 32
+            } else {
+                middleTemp = inputTemp
+            }
+            
+            if(outputTempratureFormat == "°C"){
+               return (middleTemp - 32) * 5/9
+            } else if (outputTempratureFormat == "K"){
+                return (middleTemp + 459.67) * 5/9
+            } else {
+                return middleTemp
+            }
         }
-        .padding()
+        
+        NavigationStack{
+            Form{
+                Section("Enter current Temprature"){
+                    TextField("Enter Temprature", value: $inputTemp, format: .number).keyboardType(.decimalPad)
+                    
+                    Picker("Temprature", selection: $inputTempratureFormat){
+                        ForEach(typesOfTemp, id: \.self){
+                            Text($0)
+                        }
+                    }.pickerStyle(.palette)
+                }
+                Section("Converted Temprature"){
+                    Text(outputTemp, format: .number)
+                    Picker("Temprature", selection: $outputTempratureFormat){
+                        ForEach(typesOfTemp, id:\.self){
+                            Text($0)
+                        }
+                    }                }
+                .pickerStyle(.palette)
+            }
+            
+            .navigationTitle("Temprature Converter").navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
